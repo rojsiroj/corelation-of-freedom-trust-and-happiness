fb985eae948083f061027c06254deac7a0d9015a
rojsiroj <sirojudin.dev@gmail.com>
2022-10-18 09:55:12 +0700

    update view streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,11 @@
-import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
 
 
 def getLinkSource(period):
     return 'https://www.kaggle.com/datasets/unsdsn/world-happiness?select=%s.csv' % period
 
-
-st.set_page_config(layout='wide')
 
 # Cleansing Data
 df2015 = pd.read_csv('dataset/2015.csv')
@@ -37,14 +35,32 @@ df2019 = df2019.rename(columns={'Country or region': 'country', 'Overall rank': 
                        'GDP per capita': 'economy_gdp', 'Freedom to make life choices': 'freedom', 'Perceptions of corruption': 'trust_goverment'})
 
 
+st.markdown("""
+<style>
+.text-font {
+    font-size:14pt !important;
+}
+.font-bold{
+    font-weight: bold !important;
+}
+.text-center{
+    text-align: center !important;
+}
+.text-justify{
+    text-align: justify !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
 # Streamlit
 with st.sidebar:
     selectPeriod = st.select_slider(
         "Tahun", ["2015", "2016", "2017", "2018", "2019"])
-st.title("Korelasi antara tingkat kebebasan (Freedom Score) di suatu negara dengan tingkat pendapatan (GDP) serta kesejahteraan masyarakatnya (Happiness Score) pada tahun 2015-2019".title())
-st.markdown('Kebebasan telah menjadi komponen hidup yang sangat penting terutama bagi kebanyakan _milenial_ dan _gen z_. Namun apakah keinginan manusiawi untuk meraih kebebasan (_Freedom to make life choice_) tersebut memiliki dampak terhadap ekonomi serta kesejahteraan masyarakat secara umum dan selalu memiliki korelasi yang selaras?')
+st.title("Korelasi antara tingkat kebebasan dengan tingkat kesejahteraan dan Ekonomi masyarakat".title())
+st.markdown('<p class="text-font text-justify">Kebebasan telah menjadi komponen hidup yang sangat penting terutama bagi kebanyakan <i>milenial</i> dan <i>gen z</i>. Namun apakah keinginan manusiawi untuk meraih kebebasan (<i>Freedom to make life choice</i>) tersebut memiliki dampak terhadap ekonomi serta kesejahteraan masyarakat secara umum dan selalu memiliki korelasi yang selaras?</p>', unsafe_allow_html=True)
 
-st.markdown('Mari kita lihat dalam diagram berikut:')
+st.markdown('<p class="text-font">Mari kita lihat dalam diagram berikut:</p>',
+            unsafe_allow_html=True)
 
 if (selectPeriod == '2015'):
     df = df2015
@@ -64,54 +80,63 @@ cf1, cf2 = st.columns(2)
 with cf1:
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    st.header(
-        'Korelasi antara kebebasan dan kesejahteraan masyarakat pada %s' % selectPeriod)
+    st.markdown('<p class="text-font font-bold text-center">Korelasi antara kebebasan dan kesejahteraan masyarakat pada %s</p>' %
+                selectPeriod, unsafe_allow_html=True)
     plt.scatter(df['freedom'], df['happiness_score'])
     plt.xlabel("Freedom")
     plt.ylabel("Happiness Score")
     st.write(fig)
-    st.info('Sumber: %s' % getLinkSource(selectPeriod))
 
 with cf2:
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
-    st.header(
-        'Korelasi antara kebebasan dan GDP (PDB) pada %s' % selectPeriod)
+    st.markdown('<p class="text-font font-bold text-center">Korelasi antara kebebasan dengan GDP (PDB) pada %s</p>' %
+                selectPeriod, unsafe_allow_html=True)
     plt.scatter(df['freedom'], df['economy_gdp'])
     plt.xlabel("Freedom")
     plt.ylabel("GDP")
     st.write(fig)
-    st.info('Sumber: %s' % getLinkSource(selectPeriod))
+st.info('Sumber: %s' % getLinkSource(selectPeriod))
 
-st.markdown('Menurut kedua diagram di atas, memang cenderung selaras antara tingkat kebebasan (_freedom_) dengan tingkat _GDP_ serta kesejahteraan (_happiness score_), namun ada beberapa anomali terutama yang terlihat jelas tedapat dalam beberapa data terakhir dengan tingkat _freedom_ yang tinggi namun memiliki _GDP_ serta _happiness score_ yang rendah. Mengapa hal tersebut dapat terjadi?')
+
+st.write('\n')
+st.write('\n')
+st.markdown('<p class="text-font text-justify">Menurut kedua diagram di atas, memang cenderung selaras antara tingkat kebebasan (<i>freedom</i>) dengan tingkat <i>GDP</i> serta kesejahteraan (<i>happiness score</i>), namun ada beberapa anomali terutama yang terlihat jelas tedapat dalam beberapa data terakhir dengan tingkat <i>freedom</i> yang tinggi namun memiliki <i>GDP</i> serta <i>happiness score</i> yang rendah. Mengapa hal tersebut dapat terjadi?</p>', unsafe_allow_html=True)
+st.write('\n')
+st.markdown(
+    '''<p class="text-font text-justify">Kebebasan memang diinginkan semua orang, namun ketika sesuatu yang diinginkan itu diberikan secara berlebihan, maka akan berakibat buruk. Seperti dalam segi ekonomi suatu negara yang terlalu bebas (<i>liberalis</i>) maka akan berakibat buruk terhadap perekonomian negara itu sendiri.  
+    Seperti mengutip dari <a href="https://nasional.kompas.com/read/2018/01/26/08294921/apakah-demokrasi-liberal-sungguh-menyejahterakan-masyarakat" target="_blank">kompas.com</a>, <b><i>Joseph Stiglitz</i></b> pada kuliah umum di Central Europe University, Budapest, Hungaria, awal November 2014 pernah berpendapat bahwa dengan membiarkan pasar bergerak dalam logika self regulating market (pasar bebas), maka pemerintah juga telah melebarkan kesempatan (<i>opportunity</i>) bagi kelompok-kelompok masyarakat kaya untuk terus memperkaya diri bahkan sampai mengeksploitasi kekayaan masyarakat menengah ke bawah.</p>''', unsafe_allow_html=True)
 
 st.markdown(
-    '''Kebebasan memang diinginkan semua orang, namun ketika sesuatu yang diinginkan itu diberikan secara berlebihan, maka akan berakibat buruk. Seperti dalam segi ekonomi suatu negara yang terlalu bebas (_liberalis_) maka akan berakibat buruk terhadap perekonomian negara itu sendiri.  
-    Seperti mengutip dari [kompas.com](https://nasional.kompas.com/read/2018/01/26/08294921/apakah-demokrasi-liberal-sungguh-menyejahterakan-masyarakat), **_Joseph Stiglitz_** pada kuliah umum di Central Europe University, Budapest, Hungaria, awal November 2014 pernah berpendapat bahwa dengan membiarkan pasar bergerak dalam logika self regulating market (pasar bebas), maka pemerintah juga telah melebarkan kesempatan (_opportunity_) bagi kelompok-kelompok masyarakat kaya untuk terus memperkaya diri bahkan sampai mengeksploitasi kekayaan masyarakat menengah ke bawah.''')
+    '<p class="text-font text-justify">Untuk lebih jelasnya, kita dapat melihat diagram berikut:</p>', unsafe_allow_html=True)
 
-st.markdown('Untuk lebih jelasnya, kita dapat melihat tabel berikut:')
-
+st.write('\n')
+st.write('\n')
 c1, c2 = st.columns(2)
 df2019freedom = df2019.sort_values(by='freedom', ascending=False)[['happiness_rank', 'country',
                                                                    'freedom', 'happiness_score', 'economy_gdp']]
 with c1:
-    st.header("10 Negara Dengan Freedom Score Paling Tinggi Pada Tahun 2019")
+    st.markdown('<p class="text-font text-center font-bold">10 Negara Dengan Freedom Score Paling Tinggi Pada Tahun 2019',
+                unsafe_allow_html=True)
     st.table(df2019freedom.head(10).assign(
         number=[i for i in range(1, 11)]).set_index('number'))
-    st.info('Sumber: %s' % getLinkSource(2019))
 
 with c2:
-    st.header("10 Negara Dengan Freedom Score Paling Rendah Pada Tahun 2019")
+    st.markdown('<p class="text-font text-center font-bold">10 Negara Dengan Freedom Score Paling Rendah Pada Tahun 2019',
+                unsafe_allow_html=True)
     st.table(df2019freedom.tail(10).assign(
         number=[i for i in range(1, 11)]).set_index('number'))
-    st.info('Sumber: %s' % getLinkSource(2019))
-
-st.markdown('''Dapat kita lihat contohnya pada data tahun 2019 di atas, Uzbekistan merupakan negara dengan tingkat _freedom_ tertinggi namun dalam hal kesejahteraan rakyatnya berada di posisi ke-41. Mengapa hal tersebut dapat terjadi? Apakah berarti tidak ada korelasi antara kebebasan, kesejahteraan, serta ekonomi?  
-            Perlu diketahui bahwa Uzbekistan merupakan negara paling korup ke-153 dari 180 negara hal ini [Sumber](https://www.transparency.org/en/cpi/2019/index/uzb), sedangkan negara Sweden yang merupakan perinkat ke-10 dalam _freedom score_ menempati posisi ke-4 dalam negara paling anti korup [Sumber](https://www.transparency.org/en/cpi/2019/index/swe). Itu berarti apa yang dikatakan oleh Joseph Stiglitz di atas tentang semakin bebas suatu negara maka semakin besar kesempatan orang kaya untuk mengeksploitasi masyarakat menengah ke bawah adalah benar menurut data pada 2019 tersebut.''')
+st.info('Sumber: %s' % getLinkSource(2019))
 
 
-st.header('Kesimpulan')
-st.markdown('''Berdasarkan data di atas dapat disimpulkan bahwa peningkatan _Freedom Score_ tidak selalu beriringan dengan peningkatan _GDP_ atau _Happiness Score_. Bahkan Semakin bebas suatu negara maka akan semakin rentan untuk terjadi apa yang dikhawatirkan oleh Joseph Stiglitz di atas sehingga alih-alih membuat pertumbuhan _GDP_ dan _happiness score_ terus naik malah membuatnya stagnan atau bahkan cenderung menurun. Namun semakin tidak bebas juga suatu negara maka tingkat kesejahteraan serta GDP negara tersebut akan sangat sulit untuk bertumbuh.''')
+st.markdown('''<p class="text-font text-justify">Dapat kita lihat contohnya pada data tahun 2019 di atas, Uzbekistan merupakan negara dengan tingkat <i>freedom</i> tertinggi namun dalam hal kesejahteraan rakyatnya berada di posisi ke-41. Mengapa hal tersebut dapat terjadi? Apakah berarti tidak ada korelasi antara kebebasan, kesejahteraan, serta ekonomi?  
+            Perlu diketahui bahwa Uzbekistan merupakan negara paling korup ke-153 dari 180 negara (<a href="https://www.transparency.org/en/cpi/2019/index/uzb" target="_blank">lihat sumber</a>), sedangkan negara Sweden yang merupakan perinkat ke-10 dalam <i>freedom score</i> menempati posisi ke-4 dalam negara paling anti korup (<a href="https://www.transparency.org/en/cpi/2019/index/swe" target="_blank">lihat sumber</a>). Itu berarti apa yang dikatakan oleh Joseph Stiglitz di atas tentang semakin bebas suatu negara maka semakin besar kesempatan orang kaya untuk mengeksploitasi masyarakat menengah ke bawah adalah benar menurut data pada 2019 tersebut.</p>''', unsafe_allow_html=True)
+
+st.write('\n')
+st.write('\n')
+st.markdown('<p class="text-font font-bold">Kesimpulan</p>',
+            unsafe_allow_html=True)
+st.markdown('''<p class="text-font text-justify">Berdasarkan data di atas dapat disimpulkan bahwa peningkatan <i>Freedom Score</i> tidak selalu beriringan dengan peningkatan <i>GDP</i> atau <i>Happiness Score</i>. Bahkan Semakin bebas suatu negara maka akan semakin rentan untuk terjadi apa yang dikhawatirkan oleh Joseph Stiglitz di atas sehingga alih-alih membuat pertumbuhan <i>GDP<i> dan <i>happiness score<i> terus naik malah membuatnya stagnan atau bahkan cenderung menurun. Namun semakin tidak bebas juga suatu negara maka tingkat kesejahteraan serta GDP negara tersebut akan sangat sulit untuk bertumbuh.</p>''', unsafe_allow_html=True)
 # Footer
 st.markdown(
     "<p style='text-align: center; color: grey; margin-top: 100px;'>&copy; 2022  by <a href='https://github.com/rojsiroj' target='_blank'>Muhamad Sirojudin</a> TETRIS PROA CAPSTONE PROJECT</p>", unsafe_allow_html=True)
